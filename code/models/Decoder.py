9120954a427809313ca06bee9ae354988d2aad19
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-23 17:53:29 +1000

    added comments for the decoder class

--- code/models/Decoder.py
+++ code/models/Decoder.py
@@ -26,17 +26,28 @@ class Decoder(nn.Module):
     
     def _build(self, in_channels, hidden_channels, num_resblocks, res_channels, out_channels):
         layers = [
+            # here, we want to use the same padding to keep the output size the same as the input size
+            # padding = (s(n - 1) - n + f) / 2
+            # ((n - 1) -n + 3) / 2 = 1
             nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels, kernel_size=3, stride=1, padding=1)
         ]
         layers += [
+            # here, we create num_resblocks number of residual blocks
             Residual(hidden_channels, res_channels, hidden_channels) for _ in range(num_resblocks)
         ]
         layers += [
+            # each resblock output is not wrapped by the activation function as the next block has ReLU as its first layer
+            # however, the final resblock doesn't have anything to wrap its output with an activation function
+            # therefore, here we wrap the output of the final resblock with ReLU
             nn.ReLU()
         ]
         layers += [
+            # since the encoder halves the input image size twice with the first two layers,
+            # the decoder will double the size using twice with the following layers
+            # p = (2(in - 1) + 4 - 2in) / 2 = 1
             nn.ConvTranspose2d(in_channels=hidden_channels, out_channels=hidden_channels // 2, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
+            # p = (2(in - 1) + 4 - 2in) / 2 = 1
             nn.ConvTranspose2d(in_channels=hidden_channels//2, out_channels=out_channels, kernel_size=4, stride=2, padding=1),
             nn.ReLU()
         ]
