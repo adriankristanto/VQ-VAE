55515902b2104e30b4c03413a58b1cc790c306a0
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-23 16:35:42 +1000

    fixed parameters naming for consistency

--- code/models/Residual.py
+++ code/models/Residual.py
@@ -9,28 +9,28 @@ class ResidualBlock(nn.Module):
     which are implemented as ReLU, 3x3 Conv, ReLU, 1x1 Conv
     """
 
-    def __init__(self, in_channels, num_residual_hiddens, num_hiddens):
+    def __init__(self, in_channels, res_channels, hidden_channels):
         """
             1. in_channels is the number of input channels to the first conv layer, 
-            2. num_residual_hiddens is the number of output channels of the first conv layer 
+            2. res_channels is the number of output channels of the first conv layer 
                 and the number of input channels to the second conv layer
-            3. num_hiddens is the number of output channels of the second conv layer.
+            3. hidden_channels is the number of output channels of the second conv layer.
         """
         super(ResidualBlock, self).__init__()
-        self.resblock = self._build(in_channels, num_residual_hiddens, num_hiddens)
+        self.resblock = self._build(in_channels, res_channels, hidden_channels)
     
-    def _build(self, in_channels, num_residual_hiddens, num_hiddens):
+    def _build(self, in_channels, res_channels, hidden_channels):
         resblock = nn.Sequential(
             nn.ReLU(),
             # the conv layer should use the same padding, thus,
             # NOTE: in == out == n
             # padding = (s(n - 1) - n + f) / 2
             # ((n - 1) -n + 3) / 2 = 1
-            nn.Conv2d(in_channels=in_channels, out_channels=num_residual_hiddens, kernel_size=(3, 3), stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels=in_channels, out_channels=res_channels, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.ReLU(),
             # padding = (s(out - 1) - n + f) / 2
             # ((n - 1) -n + 1) / 2 = 0
-            nn.Conv2d(in_channels=num_residual_hiddens, out_channels=num_hiddens, kernel_size=(1, 1), stride=1, padding=0, bias=False)
+            nn.Conv2d(in_channels=res_channels, out_channels=hidden_channels, kernel_size=(1, 1), stride=1, padding=0, bias=False)
         )
         return resblock
 
