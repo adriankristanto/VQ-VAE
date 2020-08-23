c0daaac9c51e9bd6c8d05c4e0502f242b2c40767
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-23 16:32:52 +1000

    added documentation to the implementation of the encoder class

--- code/models/Encoder.py
+++ code/models/Encoder.py
@@ -13,24 +13,40 @@ class Encoder(nn.Module):
     """
 
     def __init__(self, in_channels, hidden_channels, num_resblocks, res_channels):
+        """
+            in_channels: the number of channels that the input image has
+            hidden_channels: the number of channels that are used by the hidden conv layers
+            res_channels: the number of channels that are used by the residual blocks
+        """
         super(Encoder, self).__init__()
         self.layers = self._build(in_channels, hidden_channels, num_resblocks, res_channels)
     
     def _build(self, in_channels, hidden_channels, num_resblocks, res_channels):
+        # NOTE: the out_channels follows the author's implementation
         layers = [
+            # here, I assume that we want the size of the output to be half of the input size
+            # padding = (2(n/2 - 1) -n + 4) / 2 = 1
             nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels // 2, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
+            # padding = (2(n/2 - 1) -n + 4) / 2 = 1
             nn.Conv2d(in_channels=hidden_channels // 2, out_channels=hidden_channels, kernel_size=4, stride=2, padding=1),
             nn.ReLU()
         ]
         layers += [
+            # here, we want to use the same padding to keep the output size the same as the input size
+            # padding = (s(n - 1) - n + f) / 2
+            # ((n - 1) -n + 3) / 2 = 1
             nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=3, stride=1, padding=1),
             nn.ReLU()
         ]
         layers += [
+            # here, we create num_resblocks number of residual blocks
             ResidualBlock(hidden_channels, res_channels, hidden_channels) for _ in range(num_resblocks)
         ]
         layers += [
+            # each resblock output is not wrapped by the activation function as the next block has ReLU as its first layer
+            # however, the final resblock doesn't have anything to wrap its output with an activation function
+            # therefore, here we wrap the output of the final resblock with ReLU
             nn.ReLU()
         ]
         return nn.Sequential(*layers)
