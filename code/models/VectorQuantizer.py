56fc9c3556ed2829f2827d89468b75e433a1061e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-24 21:51:31 +1000

    defined the parameters to the VQ layer

--- code/models/VectorQuantizer.py
+++ code/models/VectorQuantizer.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 # https://github.com/rosinality/vq-vae-2-pytorch/blob/master/vqvae.py
 
 class VectorQuantizerEMA(nn.Module):
+
     """
     VectorQuantizer with Exponentially Moving Averages (EMA)
 
@@ -17,4 +18,21 @@ class VectorQuantizerEMA(nn.Module):
         1. The EMA update method is independent of the optimiser used to train the model.
         2. Additionally, using this method results in faster training.
     """
-    pass
+
+    def __init__(self, D, K, beta, gamma, epsilon):
+        """
+        According to the paper,
+            D: dimensionality of each embedding vector, or embedding_dim in the sonnet's implementation
+            K: the size of the discrete space (the number of embedding vectors), or num_embeddings in the sonnet's implementation
+            beta: the hyperparameter that acts as a weighting to the lost term, or commitment_cost in the sonnet's implementation
+            gamma: controls the speed of the EMA, or decay in the sonnet's implementation
+            epsilon: to avoid numerical instability (such as division by zero)
+        """
+        super(VectorQuantizerEMA, self).__init__()
+        # assign the parameters to self
+        self.D = D
+        self.K = K
+        self.beta = beta
+        self.gamma = gamma
+        self.epsilon = epsilon
+        
