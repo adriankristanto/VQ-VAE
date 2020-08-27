9bac91d673c79b3679151766c5ebe3b2c7c1975c
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-27 21:56:22 +1000

    setup the device

--- code/train_vqvae.py
+++ code/train_vqvae.py
@@ -5,3 +5,7 @@ import torchvision
 import torchvision.transforms as transforms
 import os
 from tqdm import tqdm
+from models.VQVAE import VQVAE
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Current device: {device}\n", flush=True)
