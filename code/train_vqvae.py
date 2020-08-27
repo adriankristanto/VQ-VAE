a9bcf0482028a5bc4e1f59c91a68956544e841aa
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-27 22:07:44 +1000

    added directory setup

--- code/train_vqvae.py
+++ code/train_vqvae.py
@@ -13,3 +13,18 @@ from models.VQVAE import VQVAE
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Current device: {device}\n", flush=True)
+
+# directory setup
+MAIN_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../'
+if 'generated_images' not in os.listdir(MAIN_DIR):
+    print('creating generated_images directory...', flush=True)
+    os.mkdir(MAIN_DIR + 'generated_images')
+if 'logs' not in os.listdir(MAIN_DIR):
+    print('creating logs directory...', flush=True)
+    os.mkdir(MAIN_DIR + 'logs')
+if 'reconstructed_images' not in os.listdir(MAIN_DIR):
+    print('creating reconstructed_images directory...', flush=True)
+    os.mkdir(MAIN_DIR + 'reconstructed_images')
+if 'c' not in os.listdir(MAIN_DIR):
+    print('creating saved_models directory...', flush=True)
+    os.mkdir(MAIN_DIR + 'saved_models')
