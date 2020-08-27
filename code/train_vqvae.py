aaee90e5912e84350f5a9fba725e2217cd507594
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-27 22:01:55 +1000

    fixed module not found error when VQVAE imports Encoder class

--- code/train_vqvae.py
+++ code/train_vqvae.py
@@ -1,9 +1,13 @@
+import sys
+import os
+# to avoid module not found error when VQVAE imports Encoder class
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/models/')
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-import os
 from tqdm import tqdm
 from models.VQVAE import VQVAE
 
