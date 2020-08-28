918932d9ae8959fd57b41c7dc2ee7fd5530eb215
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-28 14:38:33 +1000

    loaded celeba dataset

--- code/train_vqvae.py
+++ code/train_vqvae.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 from tqdm import tqdm
 from models.VQVAE import VQVAE
 
@@ -28,3 +29,25 @@ if 'reconstructed_images' not in os.listdir(MAIN_DIR):
 if 'saved_models' not in os.listdir(MAIN_DIR):
     print('creating saved_models directory...', flush=True)
     os.mkdir(MAIN_DIR + 'saved_models')
+
+# 1. load the data
+# reference: https://github.com/jpowie01/DCGAN_CelebA/blob/master/dataset.py
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../data/'
+BATCH_SIZE = 1
+
+train_transform = transforms.Compose([
+    # make image h == image w
+    # NOTE: original size = (218, 178)
+    transforms.CenterCrop((178, 178)),
+    # if the image is resized immediately without making h == w, the image can look distorted
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = datasets.ImageFolder(root=ROOT_DIR, transform=train_transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+
+print(f"""
+Total data: {len(trainset)}
+""", flush=True)
