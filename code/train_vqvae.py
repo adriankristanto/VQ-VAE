a9f6d450ae331ca242980f041aadc284f3c70f9f
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-06 15:52:51 +1000

    wrapped the entire code in a if __name__ == '__main__' so that the multiprocessing works with windows os

--- code/train_vqvae.py
+++ code/train_vqvae.py
@@ -12,166 +12,168 @@ import torchvision.datasets as datasets
 from tqdm import tqdm
 from models.VQVAE import VQVAE
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Current device: {device}\n", flush=True)
+if __name__ == "__main__":
 
-# directory setup
-MAIN_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../'
-if 'data' not in os.listdir(MAIN_DIR):
-    print('creating data directory...', flush=True)
-    os.mkdir(MAIN_DIR + 'data')
-if 'generated_images' not in os.listdir(MAIN_DIR):
-    print('creating generated_images directory...', flush=True)
-    os.mkdir(MAIN_DIR + 'generated_images')
-if 'logs' not in os.listdir(MAIN_DIR):
-    print('creating logs directory...', flush=True)
-    os.mkdir(MAIN_DIR + 'logs')
-if 'reconstructed_images' not in os.listdir(MAIN_DIR):
-    print('creating reconstructed_images directory...', flush=True)
-    os.mkdir(MAIN_DIR + 'reconstructed_images')
-if 'saved_models' not in os.listdir(MAIN_DIR):
-    print('creating saved_models directory...', flush=True)
-    os.mkdir(MAIN_DIR + 'saved_models')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Current device: {device}\n", flush=True)
 
-# 1. load the data
-# reference: https://github.com/jpowie01/DCGAN_CelebA/blob/master/dataset.py
-ROOT_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../data/'
-BATCH_SIZE = 128
-NUM_WORKERS = 2
+    # directory setup
+    MAIN_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../'
+    if 'data' not in os.listdir(MAIN_DIR):
+        print('creating data directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'data')
+    if 'generated_images' not in os.listdir(MAIN_DIR):
+        print('creating generated_images directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'generated_images')
+    if 'logs' not in os.listdir(MAIN_DIR):
+        print('creating logs directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'logs')
+    if 'reconstructed_images' not in os.listdir(MAIN_DIR):
+        print('creating reconstructed_images directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'reconstructed_images')
+    if 'saved_models' not in os.listdir(MAIN_DIR):
+        print('creating saved_models directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'saved_models')
 
-train_transform = transforms.Compose([
-    # make image h == image w
-    # NOTE: original size = (218, 178)
-    transforms.CenterCrop((178, 178)),
-    # if the image is resized immediately without making h == w, the image can look distorted
-    transforms.Resize((128, 128)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+    # 1. load the data
+    # reference: https://github.com/jpowie01/DCGAN_CelebA/blob/master/dataset.py
+    ROOT_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../data/'
+    BATCH_SIZE = 128
+    NUM_WORKERS = 2
 
-trainset = datasets.ImageFolder(root=ROOT_DIR, transform=train_transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+    train_transform = transforms.Compose([
+        # make image h == image w
+        # NOTE: original size = (218, 178)
+        transforms.CenterCrop((178, 178)),
+        # if the image is resized immediately without making h == w, the image can look distorted
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-print(f"""
-Total data: {len(trainset)}
-""", flush=True)
+    trainset = datasets.ImageFolder(root=ROOT_DIR, transform=train_transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 
-# 2. instantiate the model
-net = VQVAE(
-    in_channels=3,
-    hidden_channels=128,
-    num_resblocks=2,
-    res_channels=32,
-    D=64,
-    K=512,
-    beta=0.25,
-    gamma=0.99
-)
+    print(f"""
+    Total data: {len(trainset)}
+    """, flush=True)
 
-print(f"{net}\n", flush=True)
+    # 2. instantiate the model
+    net = VQVAE(
+        in_channels=3,
+        hidden_channels=128,
+        num_resblocks=2,
+        res_channels=32,
+        D=64,
+        K=512,
+        beta=0.25,
+        gamma=0.99
+    )
 
-multigpu = False
-if torch.cuda.device_count() > 1:
-    print(f'Number of GPUs: {torch.cuda.device_count()}\n', flush=True)
-    net = nn.DataParallel(net)
-    multigpu = True
+    print(f"{net}\n", flush=True)
 
-net.to(device)
+    multigpu = False
+    if torch.cuda.device_count() > 1:
+        print(f'Number of GPUs: {torch.cuda.device_count()}\n', flush=True)
+        net = nn.DataParallel(net)
+        multigpu = True
 
-# 3. define the loss function
-# this is the first loss term, which will be optimized by both the encoder and the decoder
-# the third loss term, which will be optimized by the encoder, will be returned by the vq layer
-# the second loss term will not be used here, as EMA update is used.
-reconstruction_loss = nn.MSELoss()
+    net.to(device)
 
-# 4. define the optimiser
-# the learning rate used in the original implementation by the author
-LEARNING_RATE = 3e-4
-optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+    # 3. define the loss function
+    # this is the first loss term, which will be optimized by both the encoder and the decoder
+    # the third loss term, which will be optimized by the encoder, will be returned by the vq layer
+    # the second loss term will not be used here, as EMA update is used.
+    reconstruction_loss = nn.MSELoss()
 
-# 5. train the model
-MODEL_DIRPATH = os.path.dirname(os.path.realpath(__file__)) + '/../saved_models/'
-RECONSTRUCTED_DIRPATH = os.path.dirname(os.path.realpath(__file__)) + '/../reconstructed_images/'
-CONTINUE_TRAIN = False
-CONTINUE_TRAIN_NAME = MODEL_DIRPATH + 'vqvae-model-epoch10.pth'
-EPOCH = 50
-SAVE_INTERVAL = 5
-# for reconstruction test
-RECONSTRUCTION_SIZE = 8
+    # 4. define the optimiser
+    # the learning rate used in the original implementation by the author
+    LEARNING_RATE = 3e-4
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
 
-def save_training_progress(epoch, net, optimizer, filename):
-    torch.save({
-        # since the currect epoch has been completed, save the next epoch
-        'epoch' : epoch + 1,
-        'net_state_dict' : net.state_dict(),
-        'optimizer_state_dict' : optimizer.state_dict(),
-    }, filename)
+    # 5. train the model
+    MODEL_DIRPATH = os.path.dirname(os.path.realpath(__file__)) + '/../saved_models/'
+    RECONSTRUCTED_DIRPATH = os.path.dirname(os.path.realpath(__file__)) + '/../reconstructed_images/'
+    CONTINUE_TRAIN = False
+    CONTINUE_TRAIN_NAME = MODEL_DIRPATH + 'vqvae-model-epoch10.pth'
+    EPOCH = 50
+    SAVE_INTERVAL = 5
+    # for reconstruction test
+    RECONSTRUCTION_SIZE = 8
 
-next_epoch = 0
-if CONTINUE_TRAIN:
-    checkpoint = torch.load(CONTINUE_TRAIN_NAME)
-    net.load_state_dict(checkpoint.get('net_state_dict'))
-    optimizer.load_state_dict(checkpoint.get('optimizer_state_dict'))
-    next_epoch = checkpoint.get('epoch')
+    def save_training_progress(epoch, net, optimizer, filename):
+        torch.save({
+            # since the currect epoch has been completed, save the next epoch
+            'epoch' : epoch + 1,
+            'net_state_dict' : net.state_dict(),
+            'optimizer_state_dict' : optimizer.state_dict(),
+        }, filename)
 
-# training loop
-for epoch in range(next_epoch, EPOCH):
-    # running_loss = 0.0
+    next_epoch = 0
+    if CONTINUE_TRAIN:
+        checkpoint = torch.load(CONTINUE_TRAIN_NAME)
+        net.load_state_dict(checkpoint.get('net_state_dict'))
+        optimizer.load_state_dict(checkpoint.get('optimizer_state_dict'))
+        next_epoch = checkpoint.get('epoch')
 
-    trainloader = tqdm(trainloader)
+    # training loop
+    for epoch in range(next_epoch, EPOCH):
+        # running_loss = 0.0
 
-    net.train()
-    for i, train_data in enumerate(trainloader):
-        inputs = train_data[0].to(device)
-        # 1. zeroes the gradients
-        # optimizer.zero_grad() vs net.zero_grad()
-        # reference: https://discuss.pytorch.org/t/model-zero-grad-or-optimizer-zero-grad/28426
-        net.zero_grad()
-        # 2. forward propagation
-        outputs, commitment_loss, _, _ = net(inputs)
-        # 3. compute loss
-        # note: the commitment loss has been multiplied by beta in the vq layer
-        if multigpu:
-            # if we are using multiple gpus for training, there will be x number of loss returned as a tensor 
-            # instead of just a scalar
-            # where x is the number of gpus that we used for training
-            # this can cause an issue with the autograd
-            # therefore, we need to make it a scalar by taking the mean
-            commitment_loss = torch.mean(commitment_loss)
-        mse = reconstruction_loss(outputs, inputs)
-        loss = mse + commitment_loss
-        # 4. backward propagation
-        loss.backward()
-        # 5. update parameters
-        optimizer.step()
+        trainloader = tqdm(trainloader)
 
-        # running_loss += loss.item()
+        net.train()
+        for i, train_data in enumerate(trainloader):
+            inputs = train_data[0].to(device)
+            # 1. zeroes the gradients
+            # optimizer.zero_grad() vs net.zero_grad()
+            # reference: https://discuss.pytorch.org/t/model-zero-grad-or-optimizer-zero-grad/28426
+            net.zero_grad()
+            # 2. forward propagation
+            outputs, commitment_loss, _, _ = net(inputs)
+            # 3. compute loss
+            # note: the commitment loss has been multiplied by beta in the vq layer
+            if multigpu:
+                # if we are using multiple gpus for training, there will be x number of loss returned as a tensor 
+                # instead of just a scalar
+                # where x is the number of gpus that we used for training
+                # this can cause an issue with the autograd
+                # therefore, we need to make it a scalar by taking the mean
+                commitment_loss = torch.mean(commitment_loss)
+            mse = reconstruction_loss(outputs, inputs)
+            loss = mse + commitment_loss
+            # 4. backward propagation
+            loss.backward()
+            # 5. update parameters
+            optimizer.step()
 
-        trainloader.set_description((
-            f"epoch: {epoch+1}/{EPOCH}; "
-            f"mse: {mse.item():.5f}; "
-            f"latent: {commitment_loss.item():.5f}; "
-            f"loss: {loss.item():.5f}"
-        ))
+            # running_loss += loss.item()
 
-        if i % 100 == 0:
-            net.eval()
-            sample = inputs[:RECONSTRUCTION_SIZE]
-            with torch.no_grad():
-                outputs, _, _, _ = net(sample)
-            torchvision.utils.save_image(
-                torch.cat([sample, outputs], dim=0),
-                RECONSTRUCTED_DIRPATH + f"vqvae_{epoch+1}_{i}.png",
-                nrow=RECONSTRUCTION_SIZE,
-                normalize=True,
-                range=(-1, 1)
-            )
-    
+            trainloader.set_description((
+                f"epoch: {epoch+1}/{EPOCH}; "
+                f"mse: {mse.item():.5f}; "
+                f"latent: {commitment_loss.item():.5f}; "
+                f"loss: {loss.item():.5f}"
+            ))
+
+            if i % 100 == 0:
+                net.eval()
+                sample = inputs[:RECONSTRUCTION_SIZE]
+                with torch.no_grad():
+                    outputs, _, _, _ = net(sample)
+                torchvision.utils.save_image(
+                    torch.cat([sample, outputs], dim=0),
+                    RECONSTRUCTED_DIRPATH + f"vqvae_{epoch+1}_{i}.png",
+                    nrow=RECONSTRUCTION_SIZE,
+                    normalize=True,
+                    range=(-1, 1)
+                )
+        
+        # save the model
+        if (epoch + 1) % SAVE_INTERVAL == 0:
+            save_training_progress(epoch, net, optimizer, MODEL_DIRPATH + f'vqvae-model-epoch{epoch + 1}.pth')
+        
+        # print(f"Training loss: {running_loss / len(trainloader)}", flush=True)
+
     # save the model
-    if (epoch + 1) % SAVE_INTERVAL == 0:
-        save_training_progress(epoch, net, optimizer, MODEL_DIRPATH + f'vqvae-model-epoch{epoch + 1}.pth')
-    
-    # print(f"Training loss: {running_loss / len(trainloader)}", flush=True)
-
-# save the model
-save_training_progress(epoch, net, optimizer, MODEL_DIRPATH + f'vqvae-model-epoch{epoch + 1}.pth')
+    save_training_progress(epoch, net, optimizer, MODEL_DIRPATH + f'vqvae-model-epoch{epoch + 1}.pth')
